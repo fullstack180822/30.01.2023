7255b6649108ef3108eb04b869a60577180a236e
micserv010821 <dotnet290320@gmail.com>
2023-01-30 21:04:11 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,25 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // targilim
+
+        // 1.
+        // get a String from the user (Scanner)
+        // print Capital if all the letters are in upper case
+        // print lower if all the letters are in lower case
+        // create a function that gets as an input 2 strings and prints the longer string
+
+        // 2.
+        // get an Int number from the userf
+        // create an array of 10 elements
+        // place random numbers in the array
+        // print True id the number from the user exist in the array
+
+        // 3.
+        // create a function which gets 2 arrays and print the array in which the
+        // sum of elements is higher
+        // i.e. : { 1, 3, 100 } and { 80, 90, 99 }
+        // sum :      104                269
+        // therfor will print the 2snd array == { 80, 90, 99 } -- use Array.toString
     }
 }
